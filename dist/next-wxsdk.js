11c4ee8e758a1b8a212202e1fe0230a9892522d5
afeiship <1290657123@qq.com>
2017-03-24 11:02:04 +0800

    optimize config

--- dist/next-wxsdk.js
+++ dist/next-wxsdk.js
@@ -1,15 +1,27 @@
 (function (global) {
 
   var nx = global.nx || require('next-js-core2');
-  var TYPES = ['Timeline','AppMessage','QQ','Weibo','QZone'];
   var wx = global.wx || require('wechat-jssdk');
 
   var Wxsdk = nx.declare('nx.Wxsdk', {
     statics:{
-      version:'1.2.0',
+      VERSION:'1.2.0',
+      SHARE_TAYPES:['Timeline','AppMessage','QQ','Weibo','QZone'],
+      DEFAULT_API_LIST:[
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'onMenuShareQQ',
+        'onMenuShareWeibo',
+        'onMenuShareQZone'
+      ],
       config:function(inOptions){
+        var options = nx.mix({
+          debug:true,
+          jsApiList:Wxsdk.DEFAULT_API_LIST
+        },inOptions);
+
         if (typeof wx != 'undefined') {
-          wx.config(inOptions);
+          wx.config(options);
         } else {
           nx.error('Must import this wx api script: <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" charset="utf-8"></script>')
         }
@@ -19,9 +31,10 @@
           inCallback.call(inContext);
         });
       },
-      share:function(inOptions){
-        (inTypes || TYPES).forEach(function(item){
-          var api = nx.format('onMenuShare{0}',[item]);
+      share:function(inOptions,inTypes){
+        var types = inTypes || Wxsdk.SHARE_TAYPES;
+        types.forEach(function(item){
+          var api = 'onMenuShare'+item;
           wx[api](inOptions);
         });
       }
